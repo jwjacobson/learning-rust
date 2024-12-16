572955f42dae343fc77dc949776c1e6c96a263f9
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2024-12-16 17:00:31 -0500

    Create temperature converter

---
+++ ftoc/src/main.rs
@@ -0,0 +1,60 @@
+use std::io;
+
+fn main() {
+    let fahrenheit = convert_from_f();
+
+    println!("What temperature should I convert?");
+
+    let mut input = String::new();
+    
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    let input: f32 = match input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => return,
+    };
+    
+    if fahrenheit {
+        println!("\n{}°F is equivalent to {}°C", input, f_to_c(input))
+    }
+    else {
+    println!("\n{}°C is equivalent to {}°F", input, c_to_f(input))
+    }
+}
+
+fn convert_from_f() -> bool {
+    loop {
+        println!("Convert from Fahrenheit or Celsius?");
+        
+        let mut units = String::new();
+        io::stdin()
+            .read_line(&mut units)
+            .expect("Failed to read line");
+            
+        let units = units.trim().to_lowercase();
+        
+        if units == "f" {
+            println!("Converting from Fahrenheit.");
+            return true
+        } 
+        
+        else if units == "c" {
+            println!("Converting from Celsius.");
+            return false
+        } 
+        
+        else {
+            println!("\nPlease enter F or C.");
+        }
+    }
+}
+
+fn f_to_c(f: f32) -> f32   {
+    return (f - 32.0) * 5.0/9.0
+}
+
+fn c_to_f(c: f32) -> f32   {
+    return (c * 9.0/5.0) + 32.0
+}
